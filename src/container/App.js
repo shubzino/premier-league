de264800af42d3adc16eabe981aeb304f0f87096
Khalid Ibrahim <kayibrahim@khalids-mbp-2.home>
2022-02-15 17:24:35 +0000

    updated repo

---
+++ src/container/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from '../component/Nav'
+import ShowCase from '../component/Showcase';
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <ShowCase />
+    </div>
+  );
+}
+
+export default App;
